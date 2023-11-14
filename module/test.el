f8c0e4f09faebd58d95ab76025ea6453fdff6ed9
LdBeth <andpuke@foxmail.com>
2023-11-14 12:46:31 -0600

    implement ObjC binding

---
+++ module/test.el
@@ -0,0 +1,5 @@
+(load-file "libEMT.dylib")
+(emt--tokens-range-helper "你好我好大家好")
+;; [(0 . 1) (1 . 2) (2 . 3) (3 . 4) (4 . 6) (6 . 7)]
+(emt--token-range-at-index-helper "What the fuck is this" 5)
+;; (5 . 8)
